6c2b48fff812e32c96f2a2ff6d8671451e944c87
Michael LaCroix <michael@lacroixdesign.net>
2013-09-23 13:31:03 -0500

    more performant mobile clicks/touches

--- assets/javascripts/application.js
+++ assets/javascripts/application.js
@@ -3,3 +3,13 @@
 //= require app/config
 //= require app/controllers
 //= require app/init
+
+/* globals FastClick */
+
+// enable :active state for links on touch screens
+document.addEventListener("touchstart", function(){}, true);
+
+// enable Fastclick lib for faster touch responses
+window.addEventListener('load', function() {
+  FastClick.attach(document.body);
+}, false);
--- assets/javascripts/vendor.js
+++ assets/javascripts/vendor.js
@@ -1,4 +1,6 @@
 require('vendor/modernizr.min');
 require('bower/es5-shim/es5-shim.min');
+require('vendor/eventlistener-polyfill.min');
+require('vendor/fastclick.min');
 require('vendor/angular.min');
 require('vendor/angular-route.min');
